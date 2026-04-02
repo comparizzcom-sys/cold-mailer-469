a37de4e31190630554dac6667c2681175537b3d0
Aymaan Alam <mohammedrahail007@gmail.com>
2026-04-02 20:49:26 +0530

    Add beta access note to signup

--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -3,7 +3,12 @@ import { SignUp } from "@clerk/nextjs";
 export default function SignUpPage() {
   return (
     <main className="authPage">
-      <SignUp fallbackRedirectUrl="/onboarding" />
+      <div className="authStack">
+        <div className="authNotice">
+          Beta access: contact <a href="mailto:aymaanalam.nitt@gmail.com">aymaanalam.nitt@gmail.com</a> to use Cold Mailer 469.
+        </div>
+        <SignUp fallbackRedirectUrl="/onboarding" />
+      </div>
     </main>
   );
 }
